a2db3cf4a96e9403bff907e0b5a46a7cc8a01dc8
chandannaresh <chandansutradhar2@gmail.com>
2023-05-13 12:06:59 +0530

    addded validation decorater

--- src/products/dtos/create-product.dto.ts
+++ src/products/dtos/create-product.dto.ts
@@ -1,8 +1,28 @@
-export interface CreateProductDTO {
+import { Length, IsNotEmpty, IsNumber, IsBoolean } from "class-validator";
+
+export class CreateProductDTO {
     prodId?: string;
+
+    @IsNotEmpty({
+
+    })
+    @Length(3, 20, {
+        message: 'Title must be between 3 and 20 characters'
+    })
     title: string;
+
+    @Length(3, 100)
     description: string;
+
+    @IsNotEmpty({
+        message: 'Price is required'
+    })
+    @IsNumber()
     price: number
+
+    @IsBoolean()
     outOfStock: boolean;
+
+    @IsNumber()
     quantity: number;
 }
